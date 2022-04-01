2f3f6cb19f5b1d07209966f51df6e449cd08ac78
merrillkoshy <merrillkoshy@gmail.com>
2022-04-01 18:57:49 +0400

    added wa widget

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,22 @@ import React from 'react';
 import { loadIcons } from '../utils/IconLoader';
 import type { AppProps } from 'next/app'
 import { ToastContainer } from 'react-toastify'
-// import '~bootstrap/dist/js/bootstrap'
+import WhatsAppWidget from "react-whatsapp-widget"
 loadIcons();
 
 function MyApp ( { Component, pageProps }: AppProps ) {
   return (
     <>
+      <div className='whatsapp-widget' style={{zIndex:6, position:'fixed'}}>
+      <WhatsAppWidget
+          phoneNumber="971521954262"
+          textReplyTime="Typically replies within 5 minutes"
+          companyName="Cyber Infotech"
+          message={ 
+            'Hello! 👋🏼 \n\nHow can we assist you?'
+          }
+        />
+        </div>
       <Component { ...pageProps } />
       <ToastContainer />
     </>
