a645157a49d9ecbd942329413376ed8eb50f8a55
merrillkoshy <merrillkoshy@gmail.com>
2022-04-05 18:39:06 +0400

    added gtag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -39,79 +39,21 @@ class MyDocument extends Document {
 		return (
 			<Html lang="en">
 				<Head>
-					<link
-						rel="apple-touch-icon"
-						sizes="57x57"
-						href="/assets/favicons/apple-icon-57x57.png"
+					{/* Global Site Tag (gtag.js) - Google Analytics */}
+					<script
+						async
+						src={`https://www.googletagmanager.com/gtag/js?id=G-7FLXDW4M2Y`}
 					/>
-					<link
-						rel="apple-touch-icon"
-						sizes="60x60"
-						href="/assets/favicons/apple-icon-60x60.png"
+					<script
+						dangerouslySetInnerHTML={{
+							__html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-7FLXDW4M2Y');
+          `,
+						}}
 					/>
-					<link
-						rel="apple-touch-icon"
-						sizes="72x72"
-						href="/assets/favicons/apple-icon-72x72.png"
-					/>
-					<link
-						rel="apple-touch-icon"
-						sizes="76x76"
-						href="/assets/favicons/apple-icon-76x76.png"
-					/>
-					<link
-						rel="apple-touch-icon"
-						sizes="114x114"
-						href="/assets/favicons/apple-icon-114x114.png"
-					/>
-					<link
-						rel="apple-touch-icon"
-						sizes="120x120"
-						href="/assets/favicons/apple-icon-120x120.png"
-					/>
-					<link
-						rel="apple-touch-icon"
-						sizes="144x144"
-						href="/assets/favicons/apple-icon-144x144.png"
-					/>
-					<link
-						rel="apple-touch-icon"
-						sizes="152x152"
-						href="/assets/favicons/apple-icon-152x152.png"
-					/>
-					<link
-						rel="apple-touch-icon"
-						sizes="180x180"
-						href="/assets/favicons/apple-icon-180x180.png"
-					/>
-					<link
-						rel="icon"
-						type="image/png"
-						sizes="192x192"
-						href="/assets/favicons/android-icon-192x192.png"
-					/>
-					<link
-						rel="icon"
-						type="image/png"
-						sizes="32x32"
-						href="/assets/favicons/favicon-32x32.png"
-					/>
-					<link
-						rel="icon"
-						type="image/png"
-						sizes="96x96"
-						href="/assets/favicons/favicon-96x96.png"
-					/>
-					<link
-						rel="icon"
-						type="image/png"
-						sizes="16x16"
-						href="/assets/favicons/favicon-16x16.png"
-					/>
-					<link rel="manifest" href="/assets/favicons/manifest.json" />
-					<meta name="msapplication-TileColor" content="#ffffff" />
-					<meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-					<meta name="theme-color" content="#0c5957" />
 				</Head>
 				<body>
 					<Main />
